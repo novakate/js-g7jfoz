0d7bd52a5337c82485b44417ced6897192c6f66c
novakate <nova.kate@seznam.cz>
2022-01-22 11:21:09 +0100

    3 files modified

--- ukol1.js
+++ ukol1.js
@@ -1,1 +1,18 @@
-console.log('Ahoj');
+let jmeno = 'Katka';
+let vek = 26;
+let vaha = 55;
+let vyska = 1.73;
+let BMI = vaha / vyska ** 2;
+console.log(BMI);
+console.log(
+  'Jmenuji se ' +
+    jmeno +
+    ', je mi ' +
+    vek +
+    ' let, vazim ' +
+    vaha +
+    ' kg, merim ' +
+    vyska +
+    ' m a moje BMI je ' +
+    BMI
+);
